f51f0c1ad7ad8fd88f9e944ec436ed0064f8a21b
ichbintobias2 <>
2022-04-09 11:49:08 +0200

    carving out project structure, adding first assets

---
+++ src/components/GameComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Vue.js</h1>
+
+  <img src="@/assets/logo.png" />
+  <img src="@/assets/5-Stars.png" />
+</template>
+
+<style scoped>
+  h1 {
+    text-align: center;
+  }
+
+  img {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+  }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="game">
+      <GameComponent />
+    </div>
+    <div id="review">
+      <ReviewComponent />
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import GameComponent from '@/components/GameComponent.vue'
+import ReviewComponent from '@/components/ReviewComponent.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    GameComponent, ReviewComponent
   }
 }
 </script>
+
+<style scoped>
+  div {
+    float: left;
+    border-width: 50px;
+  }
+</style>
